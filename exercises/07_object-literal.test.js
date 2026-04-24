bc2f6e56fedbfbf88014abd87cc722e13fab2d0e
Hector <hectordev4.services@gmail.com>
2026-04-24 16:52:56 +0200

    fix: SOLVED 07_object-literal.test.js

--- exercises/07_object-literal.test.js
+++ exercises/07_object-literal.test.js
@@ -1,14 +1,14 @@
 test('07_object-literal-1: can use shorthand for property names', () => {
   const  createMonster = (name, power) => {
     // Utilitza la nova sintaxi del literal d'Object, per tornar un literal que permeti passar els testos
-    // return {
-    //   type: 'Monster',
-    //   name: name,
-    //   power: power,
-    //   attack(target){
-    //     return `${this.name} attacked ${target.name}`;
-    //   }
-    // }
+    return {
+       type: 'Monster',
+       name: name,
+       power: power,
+       attack(target){
+         return `${this.name} attacked ${target.name}`;
+      }
+    }
   }
 
   const godzilla = createMonster('Godzilla', 1000)
@@ -25,6 +25,7 @@ test('07_object-literal-2: can use expressions as property names', () => {
       type,
       // afegeix una expressió com a nom de propietat on el nom de la propietat sigui el resultat de type.toUpperCase() + type.length
       // sembla forçat? Ho és... 😅
+      [type.toUpperCase()+type.length]: description,
     }
   }
 
@@ -32,7 +33,7 @@ test('07_object-literal-2: can use expressions as property names', () => {
     'Twix és una barra de xocolata feta per Mars, Inc., que consisteix en galeta amb altres ' +
     'cobertures i recobriments de confiteria. Les barres Twix es venen en parelles, tot i que també hi ha barres individuals més petites.'
   const twixType = 'twix'
-  const snickers = createCandy('twix', twixDescription)
+  const snickers = createCandy(twixType, twixDescription)
   expect(snickers.tasty).toBe(true)
   expect(snickers.type).toBe(twixType)
   expect(snickers.TWIX4).toBe(twixDescription)
