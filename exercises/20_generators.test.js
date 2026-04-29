03f29379965b0836c125b30f752350011245895e
Hector <hectordev4.services@gmail.com>
2026-04-29 10:54:23 +0200

    fix: SOLVED 20_generators.test.js

--- exercises/20_generators.test.js
+++ exercises/20_generators.test.js
@@ -2,12 +2,12 @@ test('20_generators-1: should yield objects with value and done properties', () 
   const odds = giveMeOneOddNumber()
 
   // Comprova que els valors generats tenen les propietats value i done
-  expect(odds.next().value).toBe(/* INTRODUEIX LA TEVA RESPOSTA AQUÍ */)
-  expect(odds.next().value).toBe(/* INTRODUEIX LA TEVA RESPOSTA AQUÍ */)
-  expect(odds.next().done).toBe(/* INTRODUEIX LA TEVA RESPOSTA AQUÍ */)
+  expect(odds.next().value).toBe(/* INTRODUEIX LA TEVA RESPOSTA AQUÍ */1)
+  expect(odds.next().value).toBe(/* INTRODUEIX LA TEVA RESPOSTA AQUÍ */3)
+  expect(odds.next().done).toBe(/* INTRODUEIX LA TEVA RESPOSTA AQUÍ */false)
   odds.next()
-  expect(odds.next().value).toBe(/* INTRODUEIX LA TEVA RESPOSTA AQUÍ */)
-  expect(odds.next().done).toBe(/* INTRODUEIX LA TEVA RESPOSTA AQUÍ */)
+  expect(odds.next().value).toBe(/* INTRODUEIX LA TEVA RESPOSTA AQUÍ */9)
+  expect(odds.next().done).toBe(/* INTRODUEIX LA TEVA RESPOSTA AQUÍ */true)
 
   function* giveMeOneOddNumber() {
     yield 1
@@ -34,5 +34,5 @@ test('20_generators-2: can be iterated over', () => {
     sum = sum + even
   }
 
-  expect(sum).toBe(/* INTRODUEIX LA TEVA RESPOSTA AQUÍ */)
+  expect(sum).toBe(/* INTRODUEIX LA TEVA RESPOSTA AQUÍ */20)
 })
