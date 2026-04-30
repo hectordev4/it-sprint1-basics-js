2365e2d922203f6d00c768ba6ef90749fc1cf2d5
Hector <hectordev4.services@gmail.com>
2026-04-30 11:01:04 +0200

    fix: SOLVED 25_es2023.test.js

--- exercises/25_es2023.test.js
+++ exercises/25_es2023.test.js
@@ -1,9 +1,11 @@
+import * as heelo from './es2023/ex-script.js'
+
 test('25_es2023-1: Array findLast and findLastIndex', () => {
   //Utilitza els mètodes findLast i findLastIndex per trobar l'últim número parell i el seu índex.
   const array = [1, 2, 3, 4, 5]
 
-  const lastEven = null // Resol aquí...
-  const lastEvenIndex = null // Resol aquí...
+  const lastEven = array.findLast(num => num%2===0) // Resol aquí...
+  const lastEvenIndex = array.findLastIndex(num => num%2===0) // Resol aquí...
 
   expect(lastEven).toBe(4)
   expect(lastEvenIndex).toBe(3)
@@ -11,14 +13,14 @@ test('25_es2023-1: Array findLast and findLastIndex', () => {
 
 test('25_es2023-2: Hashbangs in JavaScript files', () => {
   //Escriu un script amb un hashbang i comprova que no causa errors.
-  const script = null // Resol aquí...
+  const script =  heelo// Resol aquí...
 
   expect(() => eval(script)).not.toThrow()
 })
 
 test('25_es2023-3: Symbol.prototype.description', () => {
   //Crea un símbol amb una descripció i comprova que la descripció és correcta.
-  const symbol = null // Resol aquí...
+  const symbol = Symbol('description') // Resol aquí...
 
   expect(symbol.description).toBe('description')
 })
