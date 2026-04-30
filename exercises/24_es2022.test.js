6dc70477806abb436d486ab8107171177a0e0246
Hector <hectordev4.services@gmail.com>
2026-04-30 11:24:59 +0200

    fix: SOLVED 24_es2022.test.js

--- exercises/24_es2022.test.js
+++ exercises/24_es2022.test.js
@@ -1,12 +1,19 @@
 test('24_es2022-1: Class fields and private methods', () => {
   // Crea una classe amb un camp privat i un mètode privat. Afegeix un getter per accedir al camp privat.
   class Person {
+    #age='secret';
     // Resol aquí...
+    constructor(){
+      this.publicField = 'public';
+    }
+    getSecret(){
+      return this.#age;
+    }
   }
 
   const person = new Person()
   expect(person.getSecret()).toBe('secret')
-  expect(Person.publicField).toBe('public')
+  expect(person.publicField).toBe('public')
 })
 
 test('24_es2022-2: at() method for indexing arrays and strings', () => {
@@ -14,8 +21,8 @@ test('24_es2022-2: at() method for indexing arrays and strings', () => {
   const array = [1, 2, 3, 4]
   const string = 'hello'
 
-  const lastArrayElement = null // Resol aquí...
-  const secondLastStringChar = null // Resol aquí...
+  const lastArrayElement = array.at(array.length-1) // Resol aquí...
+  const secondLastStringChar = string.at(2) // Resol aquí...
 
   expect(lastArrayElement).toBe(4)
   expect(secondLastStringChar).toBe('l')
@@ -23,7 +30,7 @@ test('24_es2022-2: at() method for indexing arrays and strings', () => {
 
 test('24_es2022-3: Top-level await in modules', async () => {
   // Utilitza await a nivell superior per resoldre una promesa.
-  const result = null // Resol aquí...
+  const result = await Promise.resolve('Top-level await works!')// Resol aquí...
 
   expect(result).toBe('Top-level await works!')
 })
